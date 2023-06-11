db47fc5ae309d3c4398943453d7befbac127ac4f
AlexanderK <118285128+Ashen-Elish@users.noreply.github.com>
2023-06-11 20:46:20 +0500

    added unit-tests

---
+++ tests/quality_tests.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+from app import translate
+
+
+def test_translation_quality(input_text, expected_output, mode):
+    result = translate(input_text, mode=mode)
+    assert result == expected_output, f"Expected '{expected_output}', but got '{result}'"
+
+
+def run_quality_tests():
+    test_translation_quality("Emma is writing a letter.",
+                             "Эмма пишет письмо.",
+                             mode="en-ru")
+
+    test_translation_quality(
+        "Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему.",
+        "All happy families are alike; every unhappy family is unhappy in its own way.",
+        mode="ru-en")
+
+    print("All quality tests passed.")
+
+
+if __name__ == "__main__":
+    run_quality_tests()
---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+import unittest
+from app import translate
+
+class TestApp(unittest.TestCase):
+
+    def test_en_ru_translation(self):
+        input_text = "My name is Sarah, I live in London"
+        expected_output = "Меня зовут Сара, я живу в Лондоне"
+        result = translate(input_text, mode="en-ru")
+        self.assertEqual(expected_output, result)
+
+    def test_ru_en_translation(self):
+        input_text = "Привет, как дела?"
+        expected_output = "Hello, how are you doing?"
+        result = translate(input_text, mode="ru-en")
+        self.assertEqual(expected_output, result)
+
+if __name__ == "__main__":
+    unittest.main()

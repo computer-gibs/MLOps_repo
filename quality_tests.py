fd3e3ccf9c5d186432a33697632050e4942a76fa
AlexanderK <119937312+computer-gibs@users.noreply.github.com>
2023-06-12 18:07:16 +0500

    another quality tests attempt

--- quality_tests.py
+++ quality_tests.py
@@ -1,27 +1,28 @@
+import sys
 import app
+import unittest
 
-def test_translation_quality():
-    test_cases = [
-        {
-            "input": "Emma is writing a letter.",
-            "expected_output": "Эмма пишет письмо.",
-            "mode": "en-ru"
-        },
-        {
-            "input": "Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему.",
-            "expected_output": "All happy families are alike; every unhappy family is unhappy in its own way.",
-            "mode": "ru-en"
-        }
-    ]
+class TestTranslationModel(unittest.TestCase):
 
-    for test_case in test_cases:
-        input_text = test_case["input"]
-        expected_output = test_case["expected_output"]
-        mode = test_case["mode"]
+    def test_en_ru_translation(self):
+        input_text = "Emma is writing a letter."
+        expected_output = "Эмма пишет письмо."
+        translated_text = app.translate(input_text, mode="en-ru")
+        self.assertEqual(translated_text.strip(), expected_output)
 
-        translated_text = app.translate(input_text, mode)
-        assert translated_text.strip() == expected_output.strip(), f"Error in {mode} translation: Expected '{expected_output}', but got '{translated_text}'"
+    def test_ru_en_translation(self):
+        input_text = "Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему."
+        expected_output = "All happy families are alike; every unhappy family is unhappy in its own way."
+        translated_text = app.translate(input_text, mode="ru-en")
+        self.assertEqual(translated_text.strip(), expected_output)
 
-if __name__ == "__main__":
-    test_translation_quality()
-    print("All tests passed.")
+    def test_empty_input(self):
+        input_text = ""
+        expected_output = ""
+        translated_text_en_ru = app.translate(input_text, mode="en-ru")
+        translated_text_ru_en = app.translate(input_text, mode="ru-en")
+        self.assertEqual(translated_text_en_ru.strip(), expected_output)
+        self.assertEqual(translated_text_ru_en.strip(), expected_output)
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
